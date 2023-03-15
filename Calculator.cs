0cd1bf0fa0bb546d0b1a8db19e36dc8a05fc9a94
helpyn <helpynindonesianus@students.amikom.ac.id>
2023-03-15 12:16:22 +0700

    dddd

--- Calculator.cs
+++ Calculator.cs
@@ -1,7 +1,14 @@
 class Calculator
 {
-  static void Main(string[] args)
+  static void Main(string[] args
   {
-  
+    int a = 10;
+	int b = 6; 
+	
+	Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, penambahan(a, b));
+	Console.WriteLine("Hasil Penambahan: {0} - {1} = {2}", a, b, penambahan(a, b));
+	
+	
   }
-}
+  
+} 
